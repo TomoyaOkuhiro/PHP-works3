40a15f82bf81bb64c22989c1f6a50c8d22b57f1e
my_name <77881067+TomoyaOkuhiro@users.noreply.github.com>
2022-08-16 23:05:22 +0900

    initial commit
    フォーム作成

---
+++ sns1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="sns2.php" method="post">
+    名前
+    <div><input type="text" name="n"></div>
+    メッセージ
+    <div><textarea name="m" id="" cols="30" rows="10"></textarea></div>
+    <input type="submit" value="送信する">
+  </form>
+
+</body>
+
+</html>
